1a6a1ac382a2b0a3aaf878226c1fa8b400e04812
GHKIM <kevink0@naver.com>
2021-08-19 21:34:57 +0900

    create member repository

---
+++ src/main/java/com/gh/carrot/carrotmart/domain/repository/member/MemberRepository.java
@@ -0,0 +1,6 @@
+package com.gh.carrot.carrotmart.domain.repository.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<MemberRepository, Long> {
+}
