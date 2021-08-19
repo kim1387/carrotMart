60837b838b413655ae9898dc7d7fc9093ff8e82c
GHKIM <kevink0@naver.com>
2021-08-19 20:54:50 +0900

    init commit

---
+++ src/main/java/com/gh/carrot/carrotmart/CarrotMartApplication.java
@@ -0,0 +1,13 @@
+package com.gh.carrot.carrotmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotMartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrotMartApplication.class, args);
+    }
+
+}
