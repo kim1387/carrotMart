34537da1d67881cce7a57c728d38ad99095d8948
GHKIM <kevink0@naver.com>
2021-08-20 15:14:56 +0900

    사용자 서비스에 대한 구현체 클래스가 좀 더 의미있는 이름을 가지도록 수정

--- src/main/java/com/gh/carrot/carrotmart/service/member/GeneralMemberService.java
+++ src/main/java/com/gh/carrot/carrotmart/service/member/GeneralMemberService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class MemberServiceImpl implements MemberService{
+public class GeneralMemberService implements MemberService{
 
     private final MemberRepository memberRepository;
 
