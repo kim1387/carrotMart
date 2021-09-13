ec1a55c19e4a538c4d655bbf6c2fd43442f6d751
kim1387 <kevink0@naver.com>
2021-09-13 16:31:56 +0900

    로그인 체크를 위한 어노테이션 선언

--- src/main/java/com/gh/carrot/carrotmart/commons/annotation/LoginRequired.java
+++ src/main/java/com/gh/carrot/carrotmart/commons/annotation/LoginRequired.java
@@ -2,8 +2,9 @@ package com.gh.carrot.carrotmart.commons.annotation;
 
 import java.lang.annotation.*;
 
-@Target({ElementType.METHOD})
-@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD}) // Java compiler 가 annotation 이 어디에 적용될지 결정하기 위해 사용합니다. // 메서드 선언
+@Retention(RetentionPolicy.RUNTIME) //컴파일 전까지만 유효합니다. 즉, 컴파일 이후에는 사라지게 됩니다.
 @Documented
 public @interface LoginRequired {
 }
+//[참고 자료]https://sanghye.tistory.com/39
