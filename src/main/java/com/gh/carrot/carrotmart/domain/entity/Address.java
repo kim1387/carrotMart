0d68975b6f0f84955c69ae3c348afb041e572b3e
kim1387 <kevink0@naver.com>
2021-09-16 02:56:11 +0900

    위치와 주소를 저장하기 위한 임베디드 클래스 구현

---
+++ src/main/java/com/gh/carrot/carrotmart/domain/entity/Address.java
@@ -0,0 +1,19 @@
+package com.gh.carrot.carrotmart.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Address {
+    private String depth1;
+    private String depth2;
+    private String depth3;
+}
